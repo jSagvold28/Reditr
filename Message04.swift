32d555c971905c62a083593e04faaf3c86b91b7f
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-20 20:50:10 -0600

    foregroundcolor change

--- Message04.swift
+++ Message04.swift
@@ -44,7 +44,7 @@ struct Message04: View {
                 
                 Text("________________________________________________")
                     .font(.subheadline)
-                    .foregroundColor(.gray)
+                    .foregroundColor(.secondary)
             }
         }
     }
