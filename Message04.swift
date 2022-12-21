53afb446bf66e439ce3aebb6bee8679c643730e3
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-20 20:41:24 -0600

    Create Message04.swift

---
+++ Message04.swift
@@ -0,0 +1,57 @@
+//
+//  Message04.swift
+//  Reditr
+//
+//  Created by Jayce Sagvold on 12/20/22.
+//
+
+import SwiftUI
+
+struct Message04: View {
+    var body: some View {
+        
+        
+        VStack {
+            
+            Text("Thank you for joining r/askreddit. You can post in the group anytime.")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding(.bottom, 10.25)
+            
+            HStack(spacing: 10) {
+                
+                Image(systemName: "clock.badge.exclamationmark")
+                //    .font(.subheadline)
+                    .padding(.bottom, 6.5)
+                
+                Text("2w")
+                //   .padding(.horizontal, 30)
+                    .padding(.bottom, 6.5)
+                
+                // Reply to message
+                
+                Image(systemName: "arrowshape.turn.up.backward")
+                    .padding(.bottom, 6.5)
+                
+                Text("Message sent by a bot.")
+                    .font(.system(size: 13))
+                    .padding(.bottom, 6.5)
+                
+                
+            }
+            
+            VStack {
+                
+                Text("________________________________________________")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+    
+    struct Message04_Previews: PreviewProvider {
+        static var previews: some View {
+            Message04()
+        }
+    }
+}
