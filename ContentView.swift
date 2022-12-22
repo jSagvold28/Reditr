fb00a68536626c1d890a2dff10f03a649ea72793
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-21 19:32:29 -0600

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Reditr
+//
+//  Created by Jayce Sagvold on 12/18/22.
+//
+
+import SwiftUI
+
+
+// Homescreen for app
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+            Post1()
+            
+            Post2()
+            
+            Post3()
+            
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
